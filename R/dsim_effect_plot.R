a12bd08aa72e18862e6d841957f65b40a13e90c6
Gregory Smith <gregory.l.smith58@gmail.com>
2021-04-13 17:47:33 -0400

    remove space

--- R/dsim_effect_plot.R
+++ R/dsim_effect_plot.R
@@ -5,7 +5,6 @@
 #'
 #' This function plots the  simulated time series in \code{ggplot2}.
 #'
-#'
 #' @param dsim_object The output from \code{arima_ts_sim}.
 #'
 #' @param plot_output Specifies which time series should be plotted on the y-axis. There are four possible options: \code{pointwise_effect}, \code{cumulative_effect}, \code{untreated_time_series}, and \code{treated_time_series}.
