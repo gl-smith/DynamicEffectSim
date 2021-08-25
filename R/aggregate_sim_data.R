92e577b8a92f396e2f94c2a05418c005db0e3088
Gregory Smith <gregory.l.smith58@gmail.com>
2021-08-25 14:40:38 -0400

    aggregate sim data function

---
+++ R/aggregate_sim_data.R
@@ -0,0 +1,73 @@
+#' @title Dynanimic Effect Aggregation
+#'
+#' @description
+#' Aggregates the simulated time series data by user specified rolling time windows.
+#'
+#' @param dsim_object The output from \code{arima_ts_sim}.
+#' @param agg_function Specifies how to aggregate the data. Options include \code{"sum"} or \code{"mean}.
+#' @param window_size The number of consecutive time intervals to group together.
+#' @param drop_incomplete_window Drop groups that dont have a full window's worth of observations.
+
+aggregate_sim_data <- function(dsim_object,
+                               agg_function = "sum",
+                               window_size = 7,
+                               drop_incomplete_window = TRUE) {
+
+  require(assertthat)
+  require(tidyverse)
+
+  assert_that(
+    any(class(dsim_object) == "dyn-arima-sim"),
+    msg = "The input to argument one must be of class dyn-arima-sim"
+    )
+
+    # Ensures that window_size input is an integer
+    assert_that(
+      window_size %% 1 == 0,
+      msg = "The input to the argument window_size must be an integer."
+    )
+
+  # Creates data frame grouped by specified time interval
+  # Counts number of observations for each time window
+  # Drops windows with incomplete observations if specified
+
+  base_output <- tibble::as_tibble(dsim_object) %>%
+    dplyr::mutate(time_interval = group_n_consec_rows(.)) %>%
+    dplyr::group_by(time_interval) %>%
+    dplyr::mutate(values_in_window = n())
+
+
+  # Drops windows with incomplete observations if specified
+  if(drop_incomplete_window == TRUE) {
+    base_output <- dplyr::filter(base_output, values_in_window == window_size)
+  }
+
+  # Warns users if a window is incomplete
+  if(min(base_output$values_in_window) < window_size) {
+    warning("The output contains an incomplete window.")
+  }
+
+  if(agg_function == "sum") {
+    final_output <- base_output %>%
+      dplyr::summarise_at(vars(y0:cumulative_effect), sum) %>%
+      dplyr::ungroup() %>%
+      dplyr::rename(time = time_interval)
+  }
+
+  if(agg_function == "mean") {
+    final_output <- base_output %>%
+      dplyr::summarise_at(vars(y0:cumulative_effect), mean) %>%
+      dplyr::ungroup() %>%
+      dplyr::rename(time = time_interval)
+  }
+
+  final_output <- add_dsim_class(final_output)
+
+  # Species how the data was aggregated
+  output_specifications <-
+    glue::glue("Aggregation function: {agg_function}. Periods in window = {window_size}.")
+
+  print(output_specifications)
+  return(final_output)
+
+}
