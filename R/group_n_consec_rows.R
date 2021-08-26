fb5d6ab53a31a0ef546bb4859d2e6b7d4c6940a4
Gregory Smith <gregory.l.smith58@gmail.com>
2021-08-26 14:55:36 -0400

    test delete

--- R/group_n_consec_rows.R
+++
@@ -1,9 +0,0 @@
-#' Create group IDs for specified number of consecutive rows in data frames
-#'
-#' @param data Data frame
-#' @param group.size Number of consecutive rows to group together
-
-group_n_consec_rows <- function(data, group.size = 7) {
-  # + 1 ensures that the first group starts at 1
-  ((1:nrow(data) - 1) %/% group.size) + 1
-}
